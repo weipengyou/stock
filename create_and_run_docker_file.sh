02ed19d29cd61979face4e5f3cb3c9dfde6dff3d
nweill <nweill@mnubo.com>
2020-08-25 11:20:18 -0400

    adding joblib 0.16.0 to the env

--- create_and_run_docker_file.sh
+++ create_and_run_docker_file.sh
@@ -1,3 +1,4 @@
-docker build . -f  Dockerfile -t my_image
+#for local testing, you can build and run your docker image locally
+docker build . -f Dockerfile -t my_image
 #to log into the docker image:
 docker run -it my_image /bin/bash
---
+++ test/unitTest/test_model_IO.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class test_model_IO(TestCase):
+    def test_get_model_from_bucket(self):
+        self.fail()
