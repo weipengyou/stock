3ff33d60fbf6980d363b6c103af215b42bf996f7
weipengyou <friendtianfeng@hotmail.com>
2022-10-02 16:37:05 -0400

    Change project ID from "stock-ycng-228" to "my-project-ycng-228"

--- build_and_deploy_docker_image.sh
+++ build_and_deploy_docker_image.sh
@@ -1,6 +1,7 @@
 # get project-ID:
 #gcloud config get-value project
-PROJECT_ID='stock-ycng-228'
+#PROJECT_ID='stock-ycng-228'
+PROJECT_ID='my-project-ycng-228'
 ### build docker and push into GCP
 gcloud builds submit --tag gcr.io/${PROJECT_ID}/stockpredictor
 
